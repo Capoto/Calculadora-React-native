0744f33e6aea1d7f099b5b786ed3fe081de9040c
Hiago Gomes Pereira <hiagogomespereira@MacBook-Pro-de-Hiago.local>
2023-09-16 18:31:22 -0300

    Mudando a cor da calculadora

--- App.js
+++ App.js
@@ -29,10 +29,10 @@ export default function App() {
         return
       case '/': 
         setCurrentNumber((fistNumber / lastNumber).toString())
-       
-        
         return
     }
+
+  
   }
 
   function handleInput(buttonPressed){
@@ -55,6 +55,7 @@ export default function App() {
         return
       case '+/-':
         setCurrentNumber(-1*currentNumber)
+        return
       case '%':
         setCurrentNumber(currentNumber/100)
         return
@@ -78,12 +79,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#483d8b'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': 'gray'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -99,10 +100,10 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#483d8b"
   },
   resultText: {
-    color: "#282F38",
+    color: "white",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
@@ -117,17 +118,20 @@ const styles = StyleSheet.create({
   buttons: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: 'rgb(71, 12, 122)',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
     minHeight: 90,
     flex: 2,
+    
+    
   },
   textButton: {
-    color: "#7c7c7c",
+    color: "white",
     fontSize: 20,
   } 
 });
